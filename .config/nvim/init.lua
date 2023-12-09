096cb92cc75e2d313f6ba50eb5d6497b20697c2b
Jarkko Sakkinen <jarkko.sakkinen@iki.fi>
2023-12-09 19:31:14 +0200

    nvim: take keymap from nvim-lspconfig
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@iki.fi>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -102,38 +102,40 @@ vim.cmd[[colorscheme dracula-soft]]
 -- Language Server Protocol (LSP)
 --
 
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-  desc = 'LSP actions',
-  callback = function(event)
-    -- Create your keybindings here...
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    local fmt = function(cmd) return function(str) return cmd:format(str) end end
-    local buffer = vim.api.nvim_get_current_buf()
-    local lsp = fmt('<cmd>lua vim.lsp.%s<cr>')
-    local diagnostic = fmt('<cmd>lua vim.diagnostic.%s<cr>')
-
-    map('n', '<leader>K', lsp 'buf.hover()')
-    map('n', '<leader>gd', lsp 'buf.definition()')
-    map('n', '<leader>gD', lsp 'buf.declaration()')
-    map('n', '<leader>gi', lsp 'buf.implementation()')
-    map('n', '<leader>go', lsp 'buf.type_definition()')
-    map('n', '<leader>gr', lsp 'buf.references()')
-    map('n', '<leader>gs', lsp 'buf.signature_help()')
-    map('n', '<F2>', lsp 'buf.rename()')
-    map('n', '<F3>', lsp 'buf.format({async = true})')
-    map('x', '<F3>', lsp 'buf.format({async = true})')
-    map('n', '<F4>', lsp 'buf.code_action()')
-
-    if vim.lsp.buf.range_code_action then
-      map('x', '<F4>', lsp 'buf.range_code_action()')
-    else
-      map('x', '<F4>', lsp 'buf.code_action()')
-    end
-
-    map('n', '<leader>gl', diagnostic 'open_float()')
-    map('n', '<leader>[d', diagnostic 'goto_prev()')
-    map('n', '<leader>]d', diagnostic 'goto_next()')
-  end
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
 })
 
 require('mason').setup()
